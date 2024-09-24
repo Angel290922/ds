9c38f99d8a6d8c235752758e92054a4a26e68e7b
Angel290922 <144636720+Angel290922@users.noreply.github.com>
2024-09-24 13:08:17 -0600

    Update materias.js

--- materias.js
+++ materias.js
@@ -2,16 +2,72 @@
 materiasCallback({
     "materias": [
         {
-            "nombre": "Matemáticas",
-            "codigo": "MAT101",
-            "profesor": "Dr. García",
+            "nombre": "Calculo vectorial",
+            "codigo": "CV001",
+            "profesor": "Dr. Ruben Gudino Perez",
             "creditos": 5,
             "semestre": 1
         },
         {
-            "nombre": "Programación",
-            "codigo": "PRO102",
-            "profesor": "Ing. Fernández",
+            "nombre": "Programacion Distribuida",
+            "codigo": "PD002",
+            "profesor": "Ing. Jose Antonio Castaneda Osornio",
+            "creditos": 3,
+            "semestre": 7
+        },
+        {
+            "nombre": "Conmutacion y enrutamiento de redes de datos",
+            "codigo": "CE003",
+            "profesor": "Ing. Oscar Valencia Pena",
+            "creditos": 6,
+            "semestre": 2
+        },
+        {
+            "nombre": "Calculo integral",
+            "codigo": "CI004",
+            "profesor": "Ing. Gustavo Soto Jimenez",
+            "creditos": 6,
+            "semestre": 2
+        },
+        {
+            "nombre": "Programacion orientada a servicios",
+            "codigo": "PS005",
+            "profesor": "Ing. Marco antonio Cordero Dillanez",
+            "creditos": 6,
+            "semestre": 2
+        },
+        {
+            "nombre": "Taller de investigacion",
+            "codigo": "TI006",
+            "profesor": "Ing. Manuel Ortiz Cortez",
+            "creditos": 6,
+            "semestre": 2
+        },
+        {
+            "nombre": "Lenguajes y Automatas II",
+            "codigo": "LA007",
+            "profesor": "Ing. Elia Ruiz Govea",
+            "creditos": 6,
+            "semestre": 2
+        },
+        {
+            "nombre": "Programacion web",
+            "codigo": "PW008",
+            "profesor": "Ing. Jose Antonio Castaneda Osornio",
+            "creditos": 6,
+            "semestre": 2
+        },
+        {
+            "nombre": "Principios electricos y aplicasiones digitales",
+            "codigo": "PD009",
+            "profesor": "Ing. Angel Rincon Ramos",
+            "creditos": 6,
+            "semestre": 2
+        },
+        {
+            "nombre": "Calculo diferencial",
+            "codigo": "CD010",
+            "profesor": "Ing. Claudia Veronica Martinez Ramirez",
             "creditos": 6,
             "semestre": 2
         }
